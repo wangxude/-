49af5388a03bc856b0065169521feeff796ce865
王旭 <1141343619@qq.com>
2019-05-20 18:11:34 +0800

    no message

---
+++ WXSparkApp/WXSparkApp/我的/ViewController/WXSDailyRecommendViewController.h
@@ -0,0 +1,17 @@
+//
+//  WXSDailyRecommendViewController.h
+//  WXSparkApp
+//
+//  Created by 王旭 on 2019/5/20.
+//  Copyright © 2019 王旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WXSDailyRecommendViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WXSparkApp/WXSparkApp/我的/ViewController/WXSDailyRecommendViewController.m
@@ -0,0 +1,82 @@
+//
+//  WXSDailyRecommendViewController.m
+//  WXSparkApp
+//
+//  Created by 王旭 on 2019/5/20.
+//  Copyright © 2019 王旭. All rights reserved.
+//
+
+#import "WXSDailyRecommendViewController.h"
+
+@interface WXSDailyRecommendViewController ()<UICollectionViewDelegate, UICollectionViewDataSource>
+
+@property (nonatomic ,strong) UICollectionView *showImageCollectionView;
+
+@end
+
+@implementation WXSDailyRecommendViewController
+
+#pragma mark -
+
+#pragma mark - life cycle 视图的生命周期
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+#pragma mark -
+
+#pragma mark - private Methods 私有方法
+
+- (void)setUpCollectionView {
+//    self.showImageCollectionView = [[UICollectionView alloc] initWithFrame:cgrecta collectionViewLayout:<#(nonnull UICollectionViewLayout *)#>]
+}
+
+
+#pragma mark -
+
+#pragma mark - Override 复写方法
+
+
+
+#pragma mark -
+
+#pragma mark - getters and setters 设置器和访问器
+
+
+
+#pragma mark -
+
+#pragma mark - UITableViewDelegate
+
+
+
+#pragma mark -
+
+#pragma mark - CustomDelegate 自定义的代理
+
+
+
+#pragma mark -
+
+#pragma mark - event response 所有触发的事件响应 按钮、通知、分段控件等
+
+
+
+#pragma mark -
+
+#pragma mark - StatisticsLog 各种页面统计Log
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
